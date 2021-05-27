c74c7b0d423dc3cc8a454935e8b5fc92eeaa82a1
sheren390 <61457923+sheren390@users.noreply.github.com>
2021-05-27 05:28:51 +0200

    first part is done (isa)

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import os
 import sys
 import librosa
 from pydub import AudioSegment
-from tempfile import mktemp
 import librosa.display
 import numpy as np
-from PIL import Image
-# import imagehash
+from tempfile import mktemp
 import pylab
+from PIL import Image
 
 
 class Shazam(QtWidgets.QMainWindow):
@@ -34,58 +33,52 @@ class Shazam(QtWidgets.QMainWindow):
         self.featuresMethods = ['spectral_centroid', 'spectral_rolloff']
         self.features = []
 
+        self.New_window.triggered.connect(self.make_new_window)
         self.Browse_songs.triggered.connect(lambda: self.browse())
 
     def browse(self):
-        """
-        - Showing a dialog for choosing desired file
-        - Convert the loaded file into array and insert it in the system
-        - load only the first minute of any song
-        """
 
-        self.songName, notUSed = QFileDialog.getOpenFileNames(
+        self.songName, self.select_song = QFileDialog.getOpenFileNames(
             self, 'Choose the Songs', os.getenv('HOME'), "mp3(*.mp3)")
+        # print(self.songName)
 
-        for index, name in enumerate(self.songName, start=1):
+        for i, name in enumerate(self.songName, start=1):
             # extract file name
             self.paths.append(os.path.basename(name))
+        # print(name)
 
         for i in range(2):
             self.songsLabel[i].setText(self.paths[i])
 
-        # Converting mp3 to .wav formate
-
         for i in range(2):
             mp3_audio = AudioSegment.from_file(self.songName[i], format="mp3")[
-                :60000]  # read mp3
-            wname = mktemp('.wav')  # use temporary file
-            mp3_audio.export(wname, format="wav")  # convert to wav
+                :60000]  # read mp3 & take only the first 60 seconds
+            waveName = mktemp('.wav')  # use temporary file
+            mp3_audio.export(waveName, format="wav")  # convert to wav
             self.wavesongs[i], self.samplingFrequencies[i] = librosa.load(
-                wname)
-        self.resultsTable.clearContents()
+                waveName)
 
     def Mixer(self):
         mixingRatio = self.mixingSlider.value() / 100
-        self.newSong = np.add(mixingRatio *
-                              self.wavesongs[0], self.wavesongs[1] * (1-mixingRatio))
-        self.spectrogram()
+        self.newSong = mixingRatio * \
+            self.wavesongs[0] + (1-mixingRatio) * self.wavesongs[1]
+        self.sepctrogram()
 
-<<<<<<< HEAD
-    # def spectrogram(self):
-    #     D = librosa.amplitude_to_db(
-    #         np.abs(librosa.stft(self.newSong)), ref=np.max)
-    #     self.saveImage("mixingSpectrogram.png", D, "linear",
-    #                    22050)  # 22050 is the default
-    #     self.extractFeatures()
+    def sepctrogram(self):
+        spectro = librosa.amplitude_to_db(
+            np.abs(librosa.stft(self.newSong)), ref=np.max)
+        self.saveImage("mixingSpectrogram.png", spectro, "linear",
+                       22050)  # 22050 is the default
+        self.extractFeatures()
 
-    # def saveImage(self, path, viewed, y, sampleRate):
-    #     Spectro_Path = path
-    #     pylab.axis('off')  # no axis
-    #     pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[],
-    #                yticks=[])  # Remove the white edge
-    #     librosa.display.specshow(viewed, y_axis=y, sr=sampleRate)
-    #     pylab.savefig(Spectro_Path, bbox_inches=None, pad_inches=0)
-    #     pylab.close()
+    def saveImage(self, path, viewed, y, sampleRate):
+        Spectro_Path = path
+        pylab.axis('off')  # no axis
+        pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[],
+                   yticks=[])  # Remove the white edge
+        librosa.display.specshow(viewed, y_axis=y, sr=sampleRate)
+        pylab.savefig(Spectro_Path, bbox_inches=None, pad_inches=0)
+        pylab.close()
 
     def extractFeatures(self):
         self.features.append(librosa.feature.spectral_centroid(
@@ -97,26 +90,10 @@ class Shazam(QtWidgets.QMainWindow):
         for i in range(2):
             self.saveImage(self.featuresMethods[i]+'.png',
                            self.features[i].T, None, self.samplingFrequencies[0])
-=======
-    def Mixer(self):
-        mixingRatio = self.mixingSlider.value() / 100
-        self.newSong = mixingRatio * \
-            self.wavsongs[0] + (1-mixingRatio) * self.wavesongs[1]
-        self.spectrogram()
 
-    def sepctrogram(self):
-        spectro = librosa.amplitude_to_db(
-            np.abs(librosa.stft(self.newSong)), ref=np.max)
-        self.saveImage("mixingSpectrogram.png", spectro, "linear",
-                       22050)  # 22050 is the default
-        self.extractFeatures()
-        
-        
-        
     def make_new_window(self):
         self.new_window = Shazam()
         self.new_window.show()
->>>>>>> 36ff0a494bcc52fda4b529054780c1e77cbb4645
 
 
 def main():
